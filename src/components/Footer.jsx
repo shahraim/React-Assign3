ef9c722feb0eeb994bed292d930d8e5a0bddef67
shahraim <shahraim.khan27112002@gmail.com>
2023-05-30 19:25:33 +0500

    CSS changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className='container text-light bg-dark py-1'>
-      <div className='row justify-content-center'>
-        <div className='col-12 col-md-6 text-center'>
-          <p>@Shahraim-khan</p>
+    <div className=' text-light bg-dark py-1'>
+      <footer className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-12 col-md-6 text-center'>
+            <p>@Shahraim-khan</p>
+          </div>
+          <div className='col-12 col-md-6 text-center'>
+            <p>Thank you for visiting our website!</p>
+          </div>
         </div>
-        <div className='col-12 col-md-6 text-center'>
-          <p>Thank you for visiting our website!</p>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 }
 
--- src/components/nested components/Home.jsx
+++ src/components/nested components/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 function Home(props) {
     return (
-        <div className="container main">
-            <div className="row justify-content-center align-items-center">
-                <div className="col-6 col-md-6 text-center">
-                    <img src={props.title} alt="main-image" className="img-fluid" />
-                </div>
-                <div className="col-12 text-center">
-                    <h3>Check Our Deals to Buy UC</h3>
+        <div className='main'>
+            <div className="container">
+                <div className="row justify-content-center align-items-center">
+                    <div className="col-6 col-md-4 text-center">
+                        <img src={props.title} alt="main-image" className="img-fluid" />
+                    </div>
+                    <div className="col-12 text-center">
+                        <h6>Check Our Deals to Buy UC</h6>
+                    </div>
                 </div>
             </div>
         </div>
